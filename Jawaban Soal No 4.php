35ccfc0f968ea63c53c570f111c19588141b86d5
truezdale <truezdaleee@gmail.com>
2020-11-11 10:25:08 +0700

    update

--- Jawaban Soal No 4.php
+++ Jawaban Soal No 4.php
@@ -3,8 +3,8 @@
 $array = ["aku", "suka", "makan", "nasi", "padang"];	
 	
 	for($i=0; $i<count($array); $i++){
-	    for($j=$i;$j>=0;$j--){
-	    echo $array[$i];    
+	    for($j=0; $j <= $i; $j++){
+	    echo $array[$j];    
         }echo "<br>";
         
     }
@@ -12,11 +12,10 @@ $array = ["aku", "suka", "makan", "nasi", "padang"];
     $array = ["padang", "nasi", "makan", "suka", "aku"];	
 	
 	for($i=0; $i<count($array); $i++){
-	    for($j=$i;$j>=0;$j--){
-	    echo $array[$i];    
+	    for($j=0; $j <= $i; $j++){
+	    echo $array[$j];    
 	    }echo "<br>";
 	    
 	}
 	
-
 ?>
